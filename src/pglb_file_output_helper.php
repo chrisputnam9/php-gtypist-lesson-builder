638bce7d5a393f04b1cbee4aa12562d4892b56d5
Chris Putnam <chris@webfx.com>
2022-05-14 13:19:06 -0400

    Helper work

--- src/pglb_file_output_helper.php
+++ src/pglb_file_output_helper.php
@@ -22,26 +22,44 @@ class PGLB_File_Output_Helper
 		return $this->handle;
 	}
 
+	/**
+	 * Generic output data to file
+	 */
 	public function file_output($data) {
 		$handle = $this->get_handle();
 		$data = $this->pglb->stringify($data);
 		fwrite($handle, $data);
 	}
 
+	/**
+	 * Generic output line - command character and a command or line contents
+	 */
+	public function file_output_line($char=' ', $command='')
+	{
+		$this->file_output($char . ':' . $command . "\n");
+	}
+
+	/**
+	 * General helpers
+	 */
+	public function file_output_break()
+	{
+		$this->file_output("\n");
+	}
+
+	/**
+	 * Comment helpers
+	 */
 	public function file_output_comment($data)
 	{
 		$data = $this->pglb->stringify($data);
 		$this->file_output('# ' . $data . "\n");
 	}
 
-	public function file_output_break()
-	{
-		$this->file_output("\n");
-	}
-
-	public function file_output_line($char=' ', $command='')
-	{
-		$this->file_output($char . ':' . $command . "\n");
+	public function file_output_comment_header($data) {
+		$this->file_output_comment_line();
+		$this->file_output_comment($data);
+		$this->file_output_comment_line();
 	}
 
 	public function file_output_comment_line($char='-', $prefix='')
@@ -50,9 +68,38 @@ class PGLB_File_Output_Helper
 		$this->file_output_comment($string);
 	}
 
-	public function file_output_comment_header($data) {
-		$this->file_output_comment_line();
-		$this->file_output_comment($data);
-		$this->file_output_comment_line();
+	/**
+	 * Header & Instruction Helpers
+	 */
+	public function file_output_header($content) {
+		// TODO - this could maybe center somehow... or decorate...
+		$this->file_output_line('B', $content);
 	}
+
+	public function file_output_instruction($content) {
+		// TODO - this could support multi-line
+		$this->file_output_line('I', $content);
+	}
+
+	/**
+	 * Lesson / Typing Helpers
+	 */
+
+	/**
+	 * Menu / Label Helpers
+	 */
+	public function file_output_menu($menu_items, $instructions="Select section") {
+
+	}
+
+	public function file_output_menu_label($menu_id) {
+		// TODO could have this check for duplicates
+		$this->file_output_line('*', $menu_id);
+	}
+
+	public function file_output_menu_goto ($menu_id) {
+		// TODO could have this check against labels to validate in some kind of 'finalize' method
+		$this->file_output_line('G', $menu_id);
+	}
+
 }
